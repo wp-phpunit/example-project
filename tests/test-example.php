9a2249eac1adb3d83d2aa0aab1c45b22597fb9b7
Evan Mattson <me@aaemnnost.tv>
2017-12-21 21:54:22 +0200

    add test to ensure package is properly installed

--- tests/test-example.php
+++ tests/test-example.php
@@ -5,4 +5,16 @@ class Example_Test extends WP_UnitTestCase {
     function test_it_works() {
         $this->assertTrue( function_exists( 'do_action' ) );
     }
+
+    function test_wp_phpunit_is_loaded_via_composer() {
+        $this->assertStringStartsWith(
+            dirname( __DIR__ ) . '/vendor/',
+            getenv( 'WP_PHPUNIT__DIR' )
+        );
+
+        $this->assertStringStartsWith(
+            dirname( __DIR__ ) . '/vendor/',
+            ( new ReflectionClass( 'WP_UnitTestCase' ) )->getFileName()
+        );
+    }
 }
